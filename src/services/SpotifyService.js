a8ddfe27722b23861085fafecbbcc6fa56e5d55e
Mayank Sanganeria <mayank10@fb.com>
2022-03-22 23:35:00 -0700

    package spotify api

--- src/services/SpotifyService.js
+++ src/services/SpotifyService.js
@@ -1,6 +1,9 @@
+import SpotifyWebApi from 'spotify-web-api-js';
+
 export default class SpotifyService {
 	constructor() {
         this.spotifyApi = new SpotifyWebApi();
+        console.log(this.spotifyApi)
         this.spotifyApi.setAccessToken("BQDWcRzx4JGUUtK8cE5JKs_I8yCW_wZ_a09PiMK4sFDBDztCiWgDNL7eSL3ZaRnI2PMBf0GTvp3DFl6IwqU3fiuhTSoHm_sq_ywkWPvRJQKrLZD4G2fiTURoWOMcaDSgxgGvz_1GIOBv3t4pLZc");
 	}
 
