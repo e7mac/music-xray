167ae36ae02d9c6427787565386199046bde5364
Mayank Sanganeria <mayank.ot@gmail.com>
2023-08-20 09:12:53 -0700

    modern js

--- src/components/ChordChart.js
+++ src/components/ChordChart.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-export default class ChordChart extends React.Component {
-	constructor(props) {
-		super(props);
-		this.canvasRef = React.createRef();
-        this.canvasClicked = this.canvasClicked.bind(this);
-	}
+const ChordChart = ({ player }) => {
+    const canvasRef = useRef(null);
 
-    canvasClicked(event) {
+    const canvasClicked = (event) => {
         const x = event.offsetX;
         const y = event.offsetY;
 
@@ -17,31 +13,35 @@ export default class ChordChart extends React.Component {
 
         const measureX = Math.floor(x / (measureWidth + padding));
         const measureY = Math.floor(y / (measureHeight + padding));
-        const measureIndex = measureY * 4 + measureX + 1 // 1-index;
+        const measureIndex = measureY * 4 + measureX + 1; // 1-index
 
-        this.props.player.seekToMeasure(measureIndex);
-    }
+        player.seekToMeasure(measureIndex);
+    };
 
-	componentDidMount() {
-        const canvas = this.canvasRef.current
+    useEffect(() => {
+        const canvas = canvasRef.current;
         canvas.width = 440;
         canvas.height = 1400;
-        canvas.addEventListener('click', this.canvasClicked, false);
+        canvas.addEventListener('click', canvasClicked, false);
 
-        const selfClass = this;
-        selfClass.props.player.refreshCurrentTrack().then((blank) => {
-            const player = selfClass.props.player;
-            clearInterval(selfClass.drawInterval);
-            selfClass.drawInterval = setInterval(() => {
-                selfClass.drawCanvas(player.songStructure, player.getCurrentTime());
+        let drawInterval;
+
+        player.refreshCurrentTrack().then(() => {
+            drawInterval = setInterval(() => {
+                drawCanvas(player.songStructure, player.getCurrentTime());
             }, 100);
-        })
-	}
+        });
 
-    drawCanvas(beats, currentTime) {
-        var totalMeasures = beats[beats.length-1].measureNumber;
+        return () => {
+            clearInterval(drawInterval);
+            canvas.removeEventListener('click', canvasClicked, false);
+        };
+    }, [player]);
 
-		const canvas = this.canvasRef.current
+    const drawCanvas = (beats, currentTime) => {
+        const totalMeasures = beats[beats.length - 1].measureNumber;
+
+        const canvas = canvasRef.current;
 
         const measuresPerRow = 4;
         const rows = Math.ceil(totalMeasures / measuresPerRow);
@@ -50,53 +50,43 @@ export default class ChordChart extends React.Component {
         const measureHeight = 30;
         const padding = 10;
 
-        let ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d');
 
-        var currentMeasure = 0;
-        for (const index in beats) {
-            const beat = beats[index];
+        let currentMeasure = 0;
+        for (const beat of beats) {
             if (beat.timestamp < currentTime) {
                 currentMeasure = beat.measureNumber;
             }
         }
 
-        for (var i=0; i<measuresPerRow; i++) {
-            for (var j=0; j<rows; j++) {
-                const measureNumber = i + j*measuresPerRow + 1; //index-from-1
+        for (let i = 0; i < measuresPerRow; i++) {
+            for (let j = 0; j < rows; j++) {
+                const measureNumber = i + j * measuresPerRow + 1; // index-from-1
                 if (measureNumber < totalMeasures) {
-                    const x = i*(measureWidth+padding);
-                    const y = j*(measureHeight+padding);
-                    var chords = "";
-                    for (const index in beats) {
-                        const beat = beats[index];
-                        if (beat.measureNumber === measureNumber) {
-                            if (beat.chord !== undefined) {
-                                chords = chords + " " + beat.chord;
-                            }
+                    const x = i * (measureWidth + padding);
+                    const y = j * (measureHeight + padding);
+                    let chords = '';
+                    for (const beat of beats) {
+                        if (beat.measureNumber === measureNumber && beat.chord !== undefined) {
+                            chords = chords + ' ' + beat.chord;
                         }
                     }
-                    var measureColor = 'yellow';
-                    if (measureNumber == currentMeasure) {
-                        measureColor = 'red';
-                    }
-                    if (measureNumber < currentMeasure) {
-                        measureColor = 'orange';
-                    }
+                    const measureColor = measureNumber === currentMeasure ? 'red' : measureNumber < currentMeasure ? 'orange' : 'yellow';
                     ctx.fillStyle = measureColor;
                     ctx.fillRect(x, y, measureWidth, measureHeight);
                     ctx.fillStyle = '#000000';
-                    ctx.font = "20px Impact";
-                    ctx.fillText(chords, x, y + 20 );
+                    ctx.font = '20px Impact';
+                    ctx.fillText(chords, x, y + 20);
                 }
             }
         }
-    }
+    };
 
-	render() {
-		return (
+    return (
         <div>
-            <canvas ref={this.canvasRef}></canvas>
+            <canvas ref={canvasRef}></canvas>
         </div>
-		);
-	}
-}
+    );
+};
+
+export default ChordChart;
