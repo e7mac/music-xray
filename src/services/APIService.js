fb6d5c2a338d1e6bfadbbb47b7db57f7221a3088
Mayank Sanganeria <mayank.ot@gmail.com>
2023-08-20 08:52:27 -0700

    async/await terminology

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -3,70 +3,61 @@ export default class APIService {
 		this.baseUrl = "https://real-music-learning-tools-data.s3.amazonaws.com/"
 	}
 
-	getBeats(track, artist, album) {
-        const path = `${this.urlConstruct(track, artist, album)}-beats.json`
-		return this.apiCall(path)
-			.then(response => {
-				return response
-			})
-			.catch(error => console.log("error: " + error));
-	}
+	async getBeats(track, artist, album) {
+        const path = `${this.urlConstruct(track, artist, album)}-beats.json`;
+        try {
+            const response = await this.apiCall(path);
+            return response;
+        } catch (error) {
+            console.error("Error:", error);
+            return null;
+        }
+    }
 
-	getChords(track, artist, album) {
-        const path = `${this.urlConstruct(track, artist, album)}-chords.json`
-		return this.apiCall(path)
-			.then(response => {
-				return response
-			})
-			.catch(error => console.log("error: " + error));
-	}
+	async getChords(track, artist, album) {
+        const path = `${this.urlConstruct(track, artist, album)}-chords.json`;
+        try {
+            const response = await this.apiCall(path);
+            return response;
+        } catch (error) {
+            console.error("Error:", error);
+            return null;
+        }
+    }
 
-	getChart(track, artist, album) {
-        const path = `${this.urlConstruct(track, artist, album)}-chart.json`
-		return this.apiCall(path)
-			.then(response => {
-				return response
-			})
-			.catch(error => console.log("error: " + error));
-	}
-
-	getSongStructure(track, artist, album) {
-		return new Promise((resolve, reject) => {
-			const selfClass = this;
-			this.getChords(track, artist, album)
-				.then((chords) => {
-					selfClass.getBeats(track, artist, album)
-						.then((beats) => {
-							var songStructure = [];
-							var measureNumber = 0;
-							var currentChord = "";
-							const threshold = 0.0;
-							for (const i in beats) {
-								const beat = beats[i];
-								if (chords.length > 0) {
-									if (beat[0] - threshold > chords[0]["timestamp"]) {
-										currentChord = chords[0]["chord"];
-										chords.shift();
-									} else {
-										currentChord = "";
-									}
-								}
-								if (beat[1] === 1) {
-									measureNumber += 1;
-								}
-								songStructure.push({
-									timestamp: beat[0],
-									beatNumber: beat[1],
-									chord: currentChord,
-									measureNumber: measureNumber,
-								});
-							}
-							resolve(songStructure);
-						})
-				})
-				.catch(error => console.log("error: " + error));
-		});
-	}
+	async getSongStructure(track, artist, album) {
+        try {
+            const chords = await this.getChords(track, artist, album);
+            const beats = await this.getBeats(track, artist, album);
+            const songStructure = [];
+            let measureNumber = 0;
+            let currentChord = "";
+            const threshold = 0.0;
+            for (const beat of beats) {
+                if (chords.length > 0) {
+                    if (beat[0] - threshold > chords[0]["timestamp"]) {
+                        currentChord = chords[0]["chord"];
+                        chords.shift();
+                    } else {
+                        currentChord = "";
+                    }
+                }
+                if (beat[1] === 1) {
+                    measureNumber += 1;
+                }
+                songStructure.push({
+                    timestamp: beat[0],
+                    beatNumber: beat[1],
+                    chord: currentChord,
+                    measureNumber: measureNumber,
+                });
+            }
+            return songStructure;
+        } catch (error) {
+            console.error("Error:", error);
+            return null;
+        }
+    }
 	
     getAudioURL(track, artist, album) {
         return `${this.baseUrl}${this.urlConstruct(track, artist, album)}.mp3`
@@ -75,25 +66,25 @@ export default class APIService {
         return `songs/${artist}/${album}/${artist} - ${track}`
     }
 
-	apiCall(path, params = {}, searchParams = {}, json = true) {
-		const url = new URL(`${this.baseUrl}${path}`)
-		const request = fetch(url)
-		if (json) {
-			return request
-				.then(res => {
-					return res.json()
-				})
-				.then(res => {
-					if (res["detail"] !== undefined) {
-						console.log(`LOGOUT from: ${url}`)
-						localStorage.removeItem('token');
-						window.location.reload()
-					}
-					return res
-				})
-		} else {
-			return request
-		}
-
-	}
+	async apiCall(path, params = {}, searchParams = {}, json = true) {
+        const url = new URL(`${this.baseUrl}${path}`);
+        const request = fetch(url);
+        try {
+            const response = await request;
+            if (json) {
+                const jsonResponse = await response.json();
+                if (jsonResponse["detail"] !== undefined) {
+                    console.log(`LOGOUT from: ${url}`);
+                    localStorage.removeItem("token");
+                    window.location.reload();
+                }
+                return jsonResponse;
+            } else {
+                return response;
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            return null;
+        }
+    }
 }
